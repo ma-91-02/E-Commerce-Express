d395b1500b78a09106449b47678112b61d4ada08
mahmood <mahmood@mgt.eu>
2022-11-04 14:16:39 +0300

    this is section 9 + 13

--- app.js
+++ app.js
@@ -1,24 +1,60 @@
-const path = require('path');
+require("dotenv").config();
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const errorController = require('./controllers/error');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use((req, res, next) => {
+  User.findById("6364cdbdf572ccb7c3964f66")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.kaav2jt.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Mohamed",
+          email: "Mohamed@your-ma.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(process.env.PORT || 3001);
+    // const io = require("./socket").init(server);
+    // io.on("connection", (socket) => {
+    //   console.log("add meal");
+    // });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
